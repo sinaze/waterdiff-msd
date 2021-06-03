3f76da7228a7efd50ec009366efb9b6a8911504e
Sina Zendehroud <zendehroud@physik.fu-berlin.de>
2021-06-03 19:22:16 +0200

    max_frames + xtc_read

--- main.c
+++ main.c
@@ -12,11 +12,13 @@
 #include <sys/types.h>
 
 #include "xdrfile_xtc.h"
+#include "xtc_seek.h"
 
-#define OPTSTR "f:z:n:h"
-#define USAGE_FMT  "%s [-f trajectory] [-z delta_z] [-n number of frames] [-h]\n"
+#define OPTSTR "f:z:n:s:bh"
+#define USAGE_FMT  "%s [-f trajectory] [-z delta_z] [-n number of frames] [-s stride] [-b] [-h]\n"
 #define ERR_FOPEN_INPUT  "fopen(input, r)"
 #define ERR_FOPEN_OUTPUT "fopen(output, w)"
+#define ERR_NOFNAME "-f is mandatory!\n"
 #define ERR_DO_THE_NEEDFUL "do_the_needful blew up"
 #define DEFAULT_PROGNAME "locmsd"
 
@@ -25,37 +27,42 @@ extern char *optarg;
 extern int opterr, optind;
 
 typedef struct {
-  // FILE         *input;
   float         delta_z;
-  uint64_t      nframes;
+  uint64_t      max_frames;
   char          fname[300];
-  int           batch;
+  int           stride;
 } options_t;
-
-int dumb_global_variable = -11;
 
 void usage(char *progname, int opt);
 int  do_the_needful(options_t *options);
 
 int main(int argc, char *argv[]) {
     int opt;
-    options_t options = { 0, 0, "" , 0};
+    options_t options = { 0.0, 0, "\0" , 1 };
 
     int natoms, nmol;
     int mol;
-    int molbatch = 0;
+    int molbatch = 53;
+
+    XDRFILE *xd;
+    int step;
+    float time, prec;
+    matrix box;
+    rvec *x;
+
+    int n_frames;
+    int est_nframes;
+    int64_t **offsets;
+
+    int i_frame = 0;
+    int t_frame;
 
     opterr = 0;
 
     while ((opt = getopt(argc, argv, OPTSTR)) != EOF)
        switch(opt) {
            case 'f':
-              sscanf(optarg, "%s", options.fname);
-              // if (!(options.input = fopen(optarg, "r")) ){
-              //    perror(ERR_FOPEN_INPUT);
-              //    exit(EXIT_FAILURE);
-              //    /* NOTREACHED */
-              // }
+              strcpy(options.fname, optarg);
               break;
 
            case 'z':
@@ -63,11 +70,12 @@ int main(int argc, char *argv[]) {
               break;
 
            case 'n':
-              options.nframes = (uint64_t) strtoul(optarg, NULL, 10);
+              options.max_frames = (uint64_t) strtoul(optarg, NULL, 10);
               break;
 
-           case 'b':
-              options.batch += 1;
+           case 's':
+              options.stride = atoi(optarg);
+              break;
 
            case 'h':
            default:
@@ -76,6 +84,12 @@ int main(int argc, char *argv[]) {
               break;
        }
 
+    if (options.fname[0] == '\0') {
+      perror(ERR_NOFNAME);
+      exit(EXIT_FAILURE);
+      /* NOTREACHED */
+    }
+
     if (do_the_needful(&options) != EXIT_SUCCESS) {
        perror(ERR_DO_THE_NEEDFUL);
        exit(EXIT_FAILURE);
@@ -83,20 +97,60 @@ int main(int argc, char *argv[]) {
     }
 
     printf("-------- optarg debug --------\n");
-    printf("delta_z = %f\n", options.delta_z);
-    printf("nframes = %llu\n", options.nframes);
-    printf("fname   = %s\n", options.fname);
+    printf("delta_z    = %f\n", options.delta_z);
+    printf("max_frames = %llu\n", options.max_frames);
+    printf("fname      = %s\n", options.fname);
+    printf("stride     = %d\n", options.stride);
     printf("-------- gubed gratpo --------\n\n");
 
     read_xtc_natoms(options.fname, &natoms);
     nmol = natoms / 3;
 
+    read_xtc_n_frames(options.fname, &n_frames, &est_nframes, offsets);
+
     printf("# %d atoms corresponding to %d molecules\n", natoms, nmol);
 
-    for (mol = 0; mol + molbatch <= nmol; mol+=molbatch) {
-      printf("# Reading positions of molecules %d-%d\n", mol, mol+molbatch-1);
-      fflush(stdout);
+    // for (mol = 0; mol+molbatch <= nmol; mol+=molbatch) {
+    //   printf("# Reading positions of molecules %d-%d\n", mol, mol+molbatch-1);
+    //   fflush(stdout);
+    // }
+
+    if (!(xd = xdrfile_open(options.fname, "r"))) {
+      perror(ERR_FOPEN_INPUT);
+      exit(EXIT_FAILURE);
+      /* NOTREACHED */
     }
+    x = calloc(natoms, sizeof(x[0]));
+    if ((read_xtc(xd, natoms, &step, &time, box, x, &prec))) {
+      perror(ERR_FOPEN_INPUT);
+      exit(EXIT_FAILURE);
+      /* NOTREACHED */
+    }
+
+    printf("-------- xdr debug --------\n");
+    printf("n_frames = %d\n", n_frames);
+    printf("natoms = %d\n", natoms);
+    printf("step   = %d\n", step);
+    printf("time   = %f\n", time);
+    printf("box    = [ %f, %f, %f ]\n", box[0][0], box[1][1], box[2][2]);
+    printf("prec   = %f\n", prec);
+    printf("-------- gubed rdx --------\n\n");
+
+    do {
+      if (i_frame == 1)
+        t_frame = time;
+      if (options.max_frames > 0 && i_frame >= options.max_frames) {
+        printf("\nMaximum number of frames reached.\n");
+        break;
+      }
+      if (i_frame % options.stride == 0) {
+        printf("\rReading frame %d of %d, t = %.2f ps ", i_frame+1, n_frames, time);
+        fflush(stdout);
+
+        printf("%f\n", x[0][2]);
+      }
+      i_frame++;
+    } while(!read_xtc(xd, natoms, &step, &time, box, x, &prec));
 
     return EXIT_SUCCESS;
 }
