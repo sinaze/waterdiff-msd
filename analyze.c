8e4fc892843741f7648af42e468a775e7aa45929
Sina Zendehroud <zendehroud@physik.fu-berlin.de>
2021-06-09 20:55:35 +0200

    get deltas for tau msds

--- analyze.c
+++ analyze.c
@@ -8,12 +8,18 @@
 
 int get_msd(const rvec *curr, const rvec *prev, const float l) {
   rvec x, y, z;
+  rvec x_prev, y_prev, z_prev;
+  rvec delta_r;
+  rvec delta_alpha;
+  rvec delta_phi;
 
   get_eckart(curr[0], curr[1], curr[2], l, x, y, z);
-  printf("\n");
-  print_rvec(x, "x");
-  print_rvec(y, "y");
-  print_rvec(z, "z");
+  get_eckart(prev[0], prev[1], prev[2], l, x_prev, y_prev, z_prev);
+  get_delta(curr[0], prev[0], 0.0, x, y, z, x_prev, y_prev, z_prev, l,
+            delta_r, delta_alpha, delta_phi);
+  print_rvec(delta_r, "\ndelta_r");
+  print_rvec(delta_alpha, "delta_alpha");
+  print_rvec(delta_phi, "delta_phi");
 
   return EXIT_SUCCESS;
 }
@@ -28,14 +34,14 @@ void print_rvec(const rvec x, const char *name) {
   printf("%s = [ %f %f %f ]\n", name, x[0], x[1], x[2]);
 }
 
-void get_eckart(const rvec o, const rvec h1, const rvec h2, const float l,
+void get_eckart(const rvec ow, const rvec h1, const rvec h2, const float l,
                 rvec x, rvec y, rvec z) {
   rvec oh1, oh2;
   int i;
 
   for (i = 0; i < DIM; i++) {
-    oh1[i] = fmod(h1[i] - o[i] + 1.5 * l, l) - 0.5 * l;
-    oh2[i] = fmod(h2[i] - o[i] + 1.5 * l, l) - 0.5 * l;
+    oh1[i] = pbc_corr(h1[i] - ow[i], l);
+    oh2[i] = pbc_corr(h2[i] - ow[i], l);
     z[i] = oh1[i] + oh2[i];
     x[i] = oh1[i] - oh2[i];
   }
@@ -44,4 +50,34 @@ void get_eckart(const rvec o, const rvec h1, const rvec h2, const float l,
   cblas_sscal(DIM, 1.0 / cblas_snrm2(DIM, y, INC), y, INC);
   rxcrossyz(x, y, z);
   cblas_sscal(DIM, 1.0 / cblas_snrm2(DIM, z, INC), z, INC);
+}
+
+/* TODO: pass-by-reference better? */
+float pbc_corr(const float x, const float l) {
+  return fmod(x + 1.5 * l, l) - 0.5 * l;
+}
+
+void get_delta(const rvec ow, const rvec ow_prev, const float delta_z,
+               const rvec x, const rvec y, const rvec z,
+               const rvec x_prev, const rvec y_prev, const rvec z_prev,
+               const float l,
+               rvec delta_r, rvec delta_alpha, rvec delta_phi) {
+  int i;
+
+  for (i = 0; i < DIM; i++) {
+    delta_r[i] = pbc_corr(ow[i] + delta_z * z[i]
+                          - ow_prev[i] + delta_z * z_prev[i], l);
+  }
+  delta_alpha[0] = 0.5 * (cblas_sdot(DIM, delta_r, INC, x, INC)
+                          + cblas_sdot(DIM, delta_r, INC, x_prev, INC));
+  delta_alpha[1] = 0.5 * (cblas_sdot(DIM, delta_r, INC, y, INC)
+                          + cblas_sdot(DIM, delta_r, INC, y_prev, INC));
+  delta_alpha[2] = 0.5 * (cblas_sdot(DIM, delta_r, INC, z, INC)
+                          + cblas_sdot(DIM, delta_r, INC, z_prev, INC));
+  delta_phi[0] = 0.5 * (cblas_sdot(DIM, y, INC, z_prev, INC)
+                        - cblas_sdot(DIM, z, INC, y_prev, INC));
+  delta_phi[1] = 0.5 * (cblas_sdot(DIM, z, INC, x_prev, INC)
+                        - cblas_sdot(DIM, x, INC, z_prev, INC));
+  delta_phi[2] = 0.5 * (cblas_sdot(DIM, x, INC, y_prev, INC)
+                        - cblas_sdot(DIM, y, INC, x_prev, INC));
 }
--- analyze.h
+++ analyze.h
@@ -8,3 +8,11 @@ void print_rvec(const rvec x, const char *name);
 
 void get_eckart(const rvec o, const rvec h1, const rvec h2, const float l,
                 rvec x, rvec y, rvec z);
+
+float pbc_corr(const float x, const float l);
+
+void get_delta(const rvec ow, const rvec ow_prev, const float delta_z,
+               const rvec x, const rvec y, const rvec z,
+               const rvec x_prev, const rvec y_prev, const rvec z_prev,
+               const float l,
+               rvec delta_r, rvec delta_alpha, rvec delta_phi);
